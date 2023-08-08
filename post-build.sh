fc4f112f55e3028071823cabe0d83c349803b057
Andrew D'Angelo <dangeloandrew@outlook.com>
2023-08-07 19:36:54 -0500

    Move init scripts at build time

--- post-build.sh
+++ post-build.sh
@@ -9,3 +9,12 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:-/bin/sh' ${TARGET_DIR}/etc/inittab
 fi
+
+# Move nonessential init scripts to background
+TARGET_INIT_D=${TARGET_DIR}/etc/init.d
+SCRIPTS=$(find ${TARGET_INIT_D} -type f -regex "${TARGET_INIT_D}/S[1-9].*")
+mkdir -p ${TARGET_INIT_D}/background
+for script in $SCRIPTS; do
+	mv $script ${TARGET_INIT_D}/background/
+done
+
