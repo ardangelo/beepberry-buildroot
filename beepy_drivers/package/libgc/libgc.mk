aef7d6524e7f8be1d038cf1028bf434f00a4daec
Andrew D'Angelo <dangeloandrew@outlook.com>
2024-03-12 06:25:21 -0500

    Update Buildroot to 2024.02, beepy-kbd to 2.9, libgc to 8.2.6, fix aerc build

--- beepy_drivers/package/libgc/libgc.mk
+++ beepy_drivers/package/libgc/libgc.mk
@@ -1,4 +1,4 @@
-LIBGC_VERSION = 8.2.4
+LIBGC_VERSION = 8.2.6
 LIBGC_SOURCE = libgc_$(LIBGC_VERSION).orig.tar.gz
 LIBGC_SITE = http://deb.debian.org/debian/pool/main/libg/libgc
 
--- build-image.sh
+++ build-image.sh
@@ -4,7 +4,7 @@ set -e
 
 # Download Buildroot
 if [ ! -d buildroot ]; then
-curl https://buildroot.org/downloads/buildroot-2023.02.tar.xz | tar xJ
+curl https://buildroot.org/downloads/buildroot-2024.02.tar.xz | tar xJ
 mv buildroot-* buildroot
 fi
 
@@ -15,3 +15,9 @@ cd buildroot
 make -j $(nproc) defconfig BR2_EXTERNAL=../beepy_drivers BR2_DEFCONFIG=../br_defconfig
 make -j $(nproc)
 echo "Image built at buildroot/output/images/sdcard.img"
+
+# After changing a br_defconfig option for a package, run `make <pkg>-dirclean>, then rebuild
+
+# rm -rf output/target
+# find output/ -name ".stamp_target_installed" -delete
+# rm -f output/build/host-gcc-final-*/.stamp_host_installed
